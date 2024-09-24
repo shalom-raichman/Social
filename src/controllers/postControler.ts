8244c59e1a7cf29b8dfadfbe18676cbecf1c3e6b
Shalom Raichman <shalomr1770@gmail.com>
2024-09-24 17:37:10 +0300

    contrroler templet don

--- src/controllers/postControler.ts
+++ src/controllers/postControler.ts
@@ -1,8 +1,24 @@
-import express, { Router }  from "express"
+import express, { Router, Request, Response }  from "express"
 
 const router:Router = express.Router()
 
-
+router.post("/login", async (req: Request, res: Response): Promise<void> => {
+    try {
+        
+        res.status(200).json({
+            err: false,
+            message: "I was way to lazy to change the defult message",
+            data: undefined
+        })       
+    } catch (err) {
+        res.status(400)
+        res.json({
+            err: true,
+            message: "I was way to lazy to change the defult message",
+            data: null
+        })
+    }
+})
 
 
 export default router
