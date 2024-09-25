491c3c5c6525a0ed6421bb51a96f96a3d87d6e2b
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 16:20:49 +0300

    add get all posts EP

--- src/controllers/postControler.ts
+++ src/controllers/postControler.ts
@@ -1,39 +1,47 @@
 import express, { Router, Request, Response }  from "express"
+import NewPostDTO from "../DTO/newPost"
+import PostService from "../sevices/postService"
+import Post from "../models/post"
+import { getFileData } from "../config/fileDataLayer"
 
 const router:Router = express.Router()
 
 router.get("/", async (req: Request, res: Response): Promise<void> => {
     try {
-        
+        const result: Post[] = await getFileData<Post>("posts") as Post[]
+        if(!result) throw new Error("Cent get data from file")
         res.status(200).json({
             err: false,
-            message: "I was way to lazy to change the defult message",
-            data: undefined
+            message: "here is all the posts",
+            data: result
         })       
     } catch (err) {
         res.status(400)
         res.json({
             err: true,
-            message: "I was way to lazy to change the defult message",
+            message: err,
             data: null
         })
     }
 })
 
 // protected rout
-router.post("/", async (req: Request, res: Response): Promise<void> => {
+router.post("/", async (
+    req: Request<any, any, NewPostDTO>,
+        res: Response): Promise<void> => {
     try {
-        
+        const result = await PostService.createNewPost(req.body)
+        if(!result) throw new Error("Cant Save New User to the file");
         res.status(200).json({
             err: false,
-            message: "I was way to lazy to change the defult message",
-            data: undefined
+            message: "Post saved succefuly",
+            data: req.body
         })       
     } catch (err) {
         res.status(400)
         res.json({
             err: true,
-            message: "I was way to lazy to change the defult message",
+            message: err,
             data: null
         })
     }
