3743dce4f667f5d842dd75fb293366bb4c3fd71f
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 11:20:43 +0300

    fix sayntax err on user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,14 +3,18 @@ import { v4 } from "uuid"
 class User{
     public id: string
     public followeres: string[] = []
-    public following: string[] = []
+    public following:  string[] = []
     public isLockedAccount: boolean = false
+    public token?: string
     constructor(
-        public username: string,
-        public password: string,
-        public email:    string,
-        public birthday: Date
+        public username:  string,
+        public password:  string,
+        public email:     string,
+        public birthday:  Date,
+        public avatarUrl: Date
     ) {
         this.id = v4()
     }
 }
+
+export default User
