d84e276e383d133980f8e81bfc11c7263bb651c6
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 14:48:14 +0300

    fix routing to the data folder

--- src/config/fileDataLayer.ts
+++ src/config/fileDataLayer.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises"
 
 export const getFileData = async <T> (resource: string): Promise<T[] | void> => {
     try {
-        const data: string = await fs.readFile(`${__dirname}/data/${resource}.json`, "utf-8")
+        const data: string = await fs.readFile(`${__dirname}/../../data/${resource}.json`, "utf-8")
         const parsedData: T[] = JSON.parse(data)
         return parsedData
     } catch (err) {
@@ -15,7 +15,7 @@ export const getFileData = async <T> (resource: string): Promise<T[] | void> => 
 export const saveFileData = async <T> (resource: string, data: T[]): Promise<boolean>=>{
     try {
         const stringifyData: string = JSON.stringify(data)
-        await fs.writeFile(`${__dirname}/data/${resource}.json`, stringifyData, {encoding: "utf-8"})
+        await fs.writeFile(`${__dirname}/../../data/${resource}.json`, stringifyData, {encoding: "utf-8"})
         return true
     } catch (err) {
         console.log(err);
