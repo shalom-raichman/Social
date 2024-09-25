3fc75fec8ef5a84a34d1d1ac1c1b0962635ebc73
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 11:20:10 +0300

    create post module

---
+++ src/models/post.ts
@@ -0,0 +1,19 @@
+import { v4 } from "uuid"
+
+class Post{
+    public id:        string
+    public createdAt: Date
+    public likedBy:   string[] = []
+
+    constructor(
+        public aoutorId: string,
+        public content:  string,
+        public hashtags: string,
+        public ref?:     string,
+    ) {
+        this.id = v4()
+        this.createdAt = new Date()
+    }
+}
+
+export default Post
