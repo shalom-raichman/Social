ce14ab660f59c1b9c998fd569204b3185ac852a7
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 14:54:26 +0300

    createNewUser logic added

--- src/sevices/userService.ts
+++ src/sevices/userService.ts
@@ -1,9 +1,9 @@
 import NewUserDTO from "../DTO/newUser"
-import fs from "fs/promises"
 import User from "../models/user"
+import { getFileData, saveFileData } from "../config/fileDataLayer"
 
-class userService {
-    public static async createNewUser(newUser: NewUserDTO): Promise<void>{
+export default class userService {
+    public static async createNewUser(newUser: NewUserDTO): Promise<boolean>{
         // create a new user() object
         const {username, password, email, birthday, avatarUrl} = newUser
         const user: User = new User(
@@ -12,10 +12,12 @@ class userService {
 
         // aad it to the user file
             // get the file as an array of objects
-
+        let users: User[] = await getFileData<User>("users") as User[]
+        if(!users) users = []
             // push
-
+        users.push(user)
             // save the array back to the file
+        return await saveFileData<User>("users", users)
     }
 }
 
