76a596c40782a9beb266ddbfee66547346c0db81
Shalom Raichman <shalomr1770@gmail.com>
2024-09-25 17:57:20 +0300

    added a few EPes

--- src/controllers/postControler.ts
+++ src/controllers/postControler.ts
@@ -2,7 +2,7 @@ import express, { Router, Request, Response }  from "express"
 import NewPostDTO from "../DTO/newPost"
 import PostService from "../sevices/postService"
 import Post from "../models/post"
-import { getFileData } from "../config/fileDataLayer"
+import { getFileData, saveFileData } from "../config/fileDataLayer"
 
 const router:Router = express.Router()
 
@@ -31,7 +31,7 @@ router.post("/", async (
         res: Response): Promise<void> => {
     try {
         const result = await PostService.createNewPost(req.body)
-        if(!result) throw new Error("Cant Save New User to the file");
+        if(!result) throw new Error("Cant Save New User to the file")
         res.status(200).json({
             err: false,
             message: "Post saved succefuly",
@@ -68,40 +68,106 @@ router.get("/search", async (req: Request, res: Response): Promise<void> => {
 
 router.get("/:id", async (req: Request, res: Response): Promise<void> => {
     try {
-        
+        const usersArray: Post[] = await getFileData<Post>("posts") as Post[]
+        const result: Post = usersArray.find(p => p.id == req.params.id) as Post
+        if(!result) throw new Error("Post not found");
         res.status(200).json({
             err: false,
-            message: "I was way to lazy to change the defult message",
-            data: undefined
+            message: "Here is the post you wonted",
+            data: result
         })       
     } catch (err) {
+        console.log(err);
+        
         res.status(400)
         res.json({
             err: true,
-            message: "I was way to lazy to change the defult message",
+            message: err,
             data: null
         })
     }
 })
 
 // protected rout
-router.patch("/like/:id", async (req: Request, res: Response): Promise<void> => {
+router.patch("/:id", async (req: Request, res: Response): Promise<void> => {
     try {
-        
+        const usersArray: Post[] = await getFileData<Post>("posts") as Post[]
+        const postIndex: number = usersArray.findIndex(p => p.id == req.params.id)
+        if(postIndex === -1) throw new Error("Post not found")
+        const newPost:Post = {
+            ...usersArray[postIndex],
+            ...req.body
+        }
+        const isSaved: boolean = await saveFileData<Post>("posts", usersArray)
+        if(!isSaved) throw new Error("could not save shanges");
         res.status(200).json({
             err: false,
-            message: "I was way to lazy to change the defult message",
-            data: undefined
+            message: "changes saved",
+            data: newPost
         })       
     } catch (err) {
         res.status(400)
         res.json({
             err: true,
-            message: "I was way to lazy to change the defult message",
+            message: err,
             data: null
         })
     }
 })
 
+// protected rout
+router.patch("/like/:id", async (
+    req:Request<any, any, {postId:string, userId:string}>,
+    res:Response
+    ):Promise<void> => {
+    try {
+        const {postId, userId} = req.body
+        const posts = await getFileData('posts') as Post[]
+        const post:Post = posts.find(p => p.id == postId) as Post
+        let opperationType:string
+        if (post.likedBy.includes(userId)) {
+            post.likedBy = post?.likedBy.filter(u => u != userId)
+            opperationType = 'unlike'
+        } else {
+            post?.likedBy.push(userId)
+            opperationType = 'like'
+        }
+        res.status(200).json({
+            err: false,
+            message: `You've successfully ${opperationType}d that post!`,
+            data: undefined
+        })
+    } catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        })
+    }
+})
+
+// protected rout
+router.delete("/:id", async (req: Request, res: Response): Promise<void> => {
+    try {
+        const usersArray: Post[] = await getFileData<Post>("posts") as Post[]
+        const post: Post = usersArray.find(p => p.id == req.params.id) as Post
+        if(!post) throw new Error("the post you wonted to delete dosent exists")
+        const UpdatedUsersArray: Post[] = usersArray.filter(p => p.id != req.params.id)
+        const isSaved: boolean = await saveFileData<Post>("posts", UpdatedUsersArray)
+        if(!isSaved) throw new Error("could not save shanges");
+        res.status(200).json({
+            err: false,
+            message: `${post.id} was deleted successfully`,
+            data: post
+        })       
+    } catch (err) {
+        res.status(400)
+        res.json({
+            err: true,
+            message: err,
+            data: null
+        })
+    }
+})
 
 export default router
